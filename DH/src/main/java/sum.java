52bc7da5114ab0ae8eb98f4eae060337b50a05eb
mada1425 <mdaaotebe@gmail.com>
2026-01-31 15:02:19 +0300

    Add files via upload

---
+++ DH/src/main/java/sum.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mdaao
+ */
+public class sum {
+ 
+    public static void main(String[] args) {
+        int x=5;
+        System.out.println("sum of",x);
+    }   
+}
